7c6aa0cd2e144a56c8fb7a8a92af7a659ada3d3c
Lyvon Grinafoh <matheus.loninjs@outlook.com>
2022-02-28 23:59:14 -0300

    Fixed:
    - Fixed an issue where any message sent in any group would be sent to the main group the bot was created to run on;
    
    Added:
    
    - Error messages are now sent more neatly in the console;
    
    - New design implemented in messages;

--- MyBot/MyBot.cpp
+++ MyBot/MyBot.cpp
@@ -5,6 +5,8 @@
 
 const std::string BOT_TOKEN = dpp::utility::read_file("Token.txt");
 const dpp::snowflake MY_GUILD_ID = 907527594096853002;
+const dpp::snowflake CHANNEL_1_ID = 806724162369355826;
+const dpp::snowflake CHANNEL_2_ID = 790956999323025438;
 
 int main()
 {
@@ -17,24 +19,42 @@ int main()
 
         if (ev.msg.author.is_bot() == false && ev.msg.attachments.size() > 0) {
 
-            for (size_t x = 0; x < ev.msg.attachments.size(); x++) {
 
-                ev.msg.attachments.at(x).download([str = ev.msg.attachments.at(x), &bot](const dpp::http_request_completion_t& k) {
+            if (ev.msg.channel_id == 806724162369355826) {
+                for (size_t x = 0; x < ev.msg.attachments.size(); x++) {
 
-                    if (k.error != dpp::http_error::h_success) {
+                    ev.msg.attachments.at(x).download([str = ev.msg.attachments.at(x), &bot, cid = ev.msg.channel_id, mid = ev.msg.id, username = ev.msg.author.username](const dpp::http_request_completion_t& k) {
 
-                        printf_s("Error\n");
-                        return;
+                        if (k.error != dpp::http_error::h_success) {
 
-                    }
+                            printf_s("Error\n");
+                            return;
 
-                    else {
-                        dpp::message msg(947975159053156352, "Uma imagem foi enviada.");
-                        msg.add_file(str.filename, k.body);
-                        bot.message_create(msg);
-                    }
+                        }
+                        std::string title = "`" + username + "` enviou um arquivo!";
+                        std::string description = "Link para o download do arquivo: " + str.url;
+                        dpp::embed embed = dpp::embed().
+                            set_color(0x9900ff).
+                            set_title(title).
+                            set_description(description).
+                            set_image(str.url.c_str()).
+                            set_timestamp(time(0));;
 
-                });
+                        bot.message_create(dpp::message(CHANNEL_1_ID, embed));
+                        bot.message_create(dpp::message(CHANNEL_2_ID, embed));
+                        bot.message_delete(mid, cid, [](const dpp::confirmation_callback_t& u) {
+
+                            if (u.is_error()) {
+
+                                dpp::error_info error = u.get_error();
+                                printf_s("Código de erro: %s \n", error.message.c_str());
+
+                            }
+
+                        });
+
+                    });
+                }
             }
         }
     });
